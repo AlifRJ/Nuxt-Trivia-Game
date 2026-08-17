0196f3d4ba35ffde676607f5229e315ed332ec2f
Alif Rahmat Julianda <alifrahmatjulianda@gmail.com>
2026-08-17 20:46:21 +0700

    Update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,10 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "en",
       },
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+      ],
     },
   },
 });
